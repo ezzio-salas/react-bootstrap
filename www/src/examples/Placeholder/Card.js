e6d60ef50d3855bf74ab2b8d75cb3f406e2863c4
AOS <aos@192.168.1.13>
2022-03-02 22:49:01 -0600

    Adding card ref

--- www/src/examples/Placeholder/Card.js
+++ www/src/examples/Placeholder/Card.js
@@ -2,12 +2,12 @@
   <Card style={{ width: '18rem' }}>
     <Card.Img variant="top" src="holder.js/100px180" />
     <Card.Body>
-      <Card.Title>Card Title</Card.Title>
+      <Card.Title>This is just a Card Title</Card.Title>
       <Card.Text>
-        Some quick example text to build on the card title and make up the bulk
+         An update quick example text to build on the card title and make up the bulk
         of the card's content.
       </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
+      <Button variant="primary">Go to my actions</Button>
     </Card.Body>
   </Card>
 
